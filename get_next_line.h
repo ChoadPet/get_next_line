f2f947878f7eef28382cabcea9bacd8a71d767d3
Choadpet <po15f94vetal>
2017-02-20 18:26:21 +0200

    First commit

---
+++ get_next_line.h
@@ -0,0 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpoltave <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2/20/17 6:26 PM by vpoltave              #+#    #+#             */
+/*   Updated: 2/20/17 6:26 PM by vpoltave             /*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_GET_NEXT_LINE_H
+#define GET_NEXT_LINE_GET_NEXT_LINE_H
+
+#endif //GET_NEXT_LINE_GET_NEXT_LINE_H
